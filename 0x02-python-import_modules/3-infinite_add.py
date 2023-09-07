f9f96d17f6c6fc453970ce11a5080b8a9133b8b1
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-09-07 16:00:37 -0700

    argv

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+def main():
+    arguments = sys.argv[1:]
+
+    total = sum(int(arg) for arg in arguments)
+    print(total)
+
+if __name__ == "__name__":
+    main()
