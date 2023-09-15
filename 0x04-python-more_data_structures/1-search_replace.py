fde556107cd127f3638190c141a0f75ea835f13b
Sbunawa89 <sbunawa_elt@yahoo.com>
2023-09-15 15:40:29 -0700

    search_repalce

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_1 = []
+    for x in my_list:
+        if x == search:
+            new_1.append(replace)
+        else:
+            nwe_1.appen(x)
+    return new_1
